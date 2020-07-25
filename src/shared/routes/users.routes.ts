5a7a71732d16340e8174082805e54754b09d8e86
pmapelli <pmapelli@hotmail.com>
2020-07-24 21:32:56 -0300

    feat: Separating into modules

--- src/shared/routes/users.routes.ts
+++ src/shared/routes/users.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
-import uploadConfig from '../config/upload';
+import uploadConfig from '../../config/upload';
 import ensureAuthenticate from '../middlewares/ensureAuthenticate';
-import CreateUsersService from '../services/CreateUserService';
-import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import CreateUsersService from '../../modules/users/services/CreateUserService';
+import UpdateUserAvatarService from '../../modules/users/services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
