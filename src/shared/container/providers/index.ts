d2f3f5c46eacc7d2365a22d5de850d67b903f49d
pmapelli <pmapelli@hotmail.com>
2020-08-10 07:55:42 -0300

    feat: Template engine

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,11 +3,11 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
-import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/imprementations/EtherealMailProvider';
-
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
+
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
