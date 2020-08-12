a57625da5feb8162ffb09305327b139092ba4509
pmapelli <pmapelli@hotmail.com>
2020-08-12 07:29:08 -0300

    feat: Listando dias disponíveis

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
 import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 
 interface IRequest {
@@ -32,7 +33,22 @@ class ListProviderMonthAvailabilityService {
       },
     );
 
-    return appointments;
+    const numberDaysInMonth = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberDaysInMonth },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map(day => {
+      const appointmentsInDay = appointments.filter(appointment => {
+        return getDate(appointment.date) === day;
+      });
+
+      return { day, available: appointmentsInDay.length < 10 };
+    });
+
+    return availability;
   }
 }
 
