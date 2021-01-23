680bcddc2cd838335180fb41b390a829e659f216
pmapelli <pmapelli@hotmail.com>
2021-01-23 16:34:26 -0300

    feat: cache lista de providers

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -28,8 +28,6 @@ class ListProviderAppointmentsService {
   }: IRequest): Promise<Appointment[]> {
     const cacheData = await this.cacheProvider.recover('chave');
 
-    console.log(cacheData);
-
     const appointments = await this.appointmentsRepository.findAllInDayFromProvider(
       { provider_id, month, year, day },
     );
